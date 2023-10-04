65169345f3b5dfba02d91487808e6b37486493d1
Tai Doan <taidoan@live.co.uk>
2023-10-04 15:55:13 +0100

    Put all into one function

--- app.js
+++ app.js
@@ -1,53 +1,46 @@
-let rolledNumber;
+let rolledNumber; // Store our dice roll
+const rollButton = document.getElementById("roll__button"); // Roll dice button
 
 const rollDice = () => {
+  // Roll the dice number
   let randomNumber = Math.floor(Math.random() * 6) + 1;
+  rolledNumber = randomNumber;
 
-  return (rolledNumber = randomNumber);
-};
-
-const rollButton = document.getElementById("roll__button");
-
-const diceFace = () => {
-  const newDice = document.createElement("div");
-  const dice = document.querySelector(".dice-face");
-  const diceDot = document.createElement("div");
-  const diceDotClass = "dice-dot";
-  diceDot.className = diceDotClass;
+  // Add the dice/update it
+  const newDice = document.createElement("div"); // Create the dice
+  const dice = document.querySelector(".dice-face"); // Select the dice
+  const diceDot = document.createElement("div"); // Create dice dot
+  diceDot.className = "dice-dot"; // Add class to the dot
 
   if (!dice) {
-    console.log("dice doesnt exist");
+    // if dice doesnt exist, create it
     newDice.className = "dice-face";
-
     for (let x = 0; x < rolledNumber; x++) {
+      // Create the number of dots
       newDice.appendChild(diceDot.cloneNode(true));
     }
-
     document.body.insertBefore(newDice, rollButton);
   } else {
-    dice.innerHTML = "";
-
+    // If it does exist, update it
+    dice.innerHTML = ""; // Remove existing dots
     for (let x = 0; x < rolledNumber; x++) {
       dice.appendChild(diceDot.cloneNode(true));
     }
   }
-};
 
-const rollResult = () => {
-  const newSpan = document.createElement("span");
-  newSpan.className = "roll__result";
-  const checkSpan = document.querySelector("span.roll__result");
+  // Add result text
+  const result = document.createElement("span"); // Create the result span
+  result.className = "roll__result";
+  const checkResult = document.querySelector("span.roll__result"); // Check if the result exists
 
-  if (!checkSpan) {
-    newSpan.textContent = `You rolled a ${rolledNumber}`;
-    document.body.insertBefore(newSpan, rollButton);
+  if (!checkResult) {
+    result.textContent = `You rolled a ${rolledNumber}`;
+    document.body.insertBefore(result, rollButton);
   } else {
-    checkSpan.textContent = `You rolled a ${rolledNumber}`;
+    checkResult.textContent = `You rolled a ${rolledNumber}`;
   }
 };
 
 rollButton.addEventListener("click", () => {
   rollDice();
-  diceFace();
-  rollResult();
 });
