0de158f7f83148e3c7da091e64128d187de29622
Tai Doan <taidoan@live.co.uk>
2023-10-04 13:23:16 +0100

    Added dice

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ let rolledNumber;
 
 const rollDice = () => {
   let randomNumber = Math.floor(Math.random() * 6) + 1;
+
   return (rolledNumber = randomNumber);
 };
 
 const rollButton = document.getElementById("roll__button");
+
+const diceFace = () => {
+  const newDice = document.createElement("div");
+  const dice = document.querySelector(".dice-face");
+  const diceDot = document.createElement("div");
+  const diceDotClass = "dice-dot";
+  diceDot.className = diceDotClass;
+
+  if (!dice) {
+    console.log("dice doesnt exist");
+    newDice.className = "dice-face";
+
+    for (let x = 0; x < rolledNumber; x++) {
+      newDice.appendChild(diceDot.cloneNode(true));
+    }
+
+    document.body.insertBefore(newDice, rollButton);
+  } else {
+    dice.innerHTML = "";
+
+    for (let x = 0; x < rolledNumber; x++) {
+      dice.appendChild(diceDot.cloneNode(true));
+    }
+  }
+};
 
 const rollResult = () => {
   const newSpan = document.createElement("span");
@@ -22,5 +48,6 @@ const rollResult = () => {
 
 rollButton.addEventListener("click", () => {
   rollDice();
+  diceFace();
   rollResult();
 });
